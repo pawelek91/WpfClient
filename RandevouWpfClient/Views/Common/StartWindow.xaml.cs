1ad3585dec007114c9c40989870deb4cf90e0c73
Pawel Olejko <pawel.olejko@enova.pl>
2019-04-25 09:57:56 +0200

    start window, messages window, login

---
+++ RandevouWpfClient/Views/Common/StartWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RandevouWpfClient.Views
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterUserBTN_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new RegisterView(new ViewModels.RegisterViewModel());
+            window.ShowDialog();
+        }
+    }
+}
