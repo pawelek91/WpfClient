9c238ab9050df42f968969c96276726a8063129a
Pawel Olejko <pawel.olejko@enova.pl>
2019-05-30 14:31:54 +0200

    my profile, eyes/hair color

---
+++ RandevouWpfClient/Models/Api/ApiQueryProvider.UserDetails.cs
@@ -0,0 +1,26 @@
+﻿using RandevouApiCommunication.Users.DictionaryValues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandevouWpfClient.Api
+{
+    public partial class ApiQueryProvider
+    {
+        public DictionaryItemDto[] GetAllEyesColors()
+        {
+            var udQuery =  queryProvider.GetQueryProvider<IUsersDictionaryValuesQuery>();
+            var result = udQuery.GetEyesColors(_apiKey);
+            return result;
+        }
+
+        public DictionaryItemDto[] GetAllHairColors()
+        {
+            var udQuery = queryProvider.GetQueryProvider<IUsersDictionaryValuesQuery>();
+            var result = udQuery.GetHairColors(_apiKey);
+            return result;
+        }
+    }
+}
--- RandevouWpfClient/ViewModels/MyProfileViewModel.cs
+++ RandevouWpfClient/ViewModels/MyProfileViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using RandevouApiCommunication.Users;
+using RandevouApiCommunication.Users.DictionaryValues;
 using RandevouWpfClient.ViewModels.Commands.MyProfile;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,34 @@ namespace RandevouWpfClient.ViewModels
 {
     public class MyProfileViewModel : PrimaryViewModel
     {
+        public DictionaryItemDto[] EyesColorsDictionary { get; set; }
+        public DictionaryItemDto[] HairColorsDictionary { get; set; }
+
+        private DictionaryItemDto myProfileHairColor;
+
+        public DictionaryItemDto MyProfileHairColor
+        {
+            get { return myProfileHairColor; }
+            set
+            {
+                myProfileHairColor = value;
+                MyProfile.HairColor = value.Id;
+            }
+        }
+
+        private DictionaryItemDto myProfileEyesColor;
+
+        public DictionaryItemDto MyProfileEyesColor
+        {
+            get { return myProfileEyesColor; }
+            set
+            {
+                myProfileEyesColor = value;
+                MyProfile.EyesColor = value.Id;
+            }
+        }
+
+
         private UserDetailsDto myProfile;
         public UpdateMyProfileCommand UpdateProfileCommand { get; set; }
 
@@ -30,6 +59,24 @@ namespace RandevouWpfClient.ViewModels
             MyProfile = queryProvider.GetMyProfileUserDetails();
 
             UpdateProfileCommand = new UpdateMyProfileCommand(this);
+
+            EyesColorsDictionary = queryProvider.GetAllEyesColors();
+            HairColorsDictionary = queryProvider.GetAllHairColors();
+
+            AssignyProfileEyesColor(MyProfileEyesColor);
+            AssignMyProfileHairColor(MyProfileHairColor);
+        }
+
+        private void AssignMyProfileHairColor(DictionaryItemDto myProfileHairColor)
+        {
+            if(MyProfile.HairColor.HasValue)
+                MyProfileHairColor = HairColorsDictionary.Single(x => x.Id == MyProfile.HairColor);
+        }
+
+        private void AssignyProfileEyesColor(DictionaryItemDto myProfileEyesColor)
+        {
+            if(MyProfile.EyesColor.HasValue)
+                MyProfileEyesColor = EyesColorsDictionary.Single(x => x.Id == MyProfile.EyesColor);
         }
     }
 }
